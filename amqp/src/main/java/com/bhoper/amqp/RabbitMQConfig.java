9ca19cf2dc72aed9bf8010abc877b5ba53dc180f
Andrey Solianyk <solianik123456@gmail.com>
2024-03-27 22:28:10 +0100

    Added and configured RabbitMQ

--- amqp/src/main/java/com/bhoper/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/bhoper/amqp/RabbitMQConfig.java
@@ -3,6 +3,7 @@ package com.bhoper.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -17,10 +18,18 @@ public class RabbitMQConfig {
     private final ConnectionFactory connectionFactory;
 
     @Bean
-    public AmqpTemplate amqpTemplate() {
+    public AmqpTemplate amqpTemplate() {   // this allows us to sent messages to the queue
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
         return rabbitTemplate;
+    }
+
+    @Bean   // this allows us to receive messages from the queue
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jackson2JsonMessageConverter());
+        return factory;
     }
 
     @Bean
